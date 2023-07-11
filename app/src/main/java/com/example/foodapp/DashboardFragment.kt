aaecce95a910ca1cf61a3ef4334281b2fd269afd
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-07-11 19:31:29 +0545

    adjusting the code to show livedata to view

--- app/src/main/java/com/example/foodapp/DashboardFragment.kt
+++ app/src/main/java/com/example/foodapp/DashboardFragment.kt
@@ -21,7 +21,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
-import com.example.foodapp.modal.MyRecyclerView
+import com.example.foodapp.modal.category.Category
+
 
 class DashboardFragment : Fragment() {
     private lateinit var retService: CategoryService
@@ -39,57 +40,75 @@ class DashboardFragment : Fragment() {
         retService = RetrofitInstance.getRetrofitInstance().create(CategoryService::class.java)
         binding = FragmentDashboardBinding.inflate(inflater,container, false)
 
-        val recyclerview = binding.dashRecyclerView
-        recyclerview.setBackgroundColor(Color.TRANSPARENT)
-        recyclerview.layoutManager = LinearLayoutManager(context)
-        recyclerview.adapter= MyRecyclerView()
+        val recyclerView = binding.dashRecyclerView
+        recyclerView.setBackgroundColor(Color.TRANSPARENT)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        val responseLiveData: LiveData<Response<FoodCategory>> = liveData {
+            val response = retService.getCategory()
+            emit(response)
+        }
+        responseLiveData.observe(this, Observer {
+            val foodList = it.body()?.categories?.listIterator()
+            if (foodList != null) {
+                recyclerView.adapter = MyRecyclerViewAdaptor(foodList)
+            }
+        })
+
+        print("************************************88")
+
 //        getRequestWithParameters()
         return binding.root
     }
 
-    private fun getRequestWithParameters(){
+    private fun getRequestWithParameters() {
 
         val responseLiveData: LiveData<Response<FoodCategory>> = liveData {
             val response = retService.getCategory()
             emit(response)
         }
+//        responseLiveData.observe(this, Observer {
+//            val foodList = it.body()?.categories?.listIterator()
+//            if (foodList != null) {
+//                return foodList
+//            }
+//        })
+//        return null
 
 //        var cat_view0  = binding.catThumb0
 //        var cat_title0 = binding.catTitle0
 //        var i = 0
 //        Log.i("MYTAG", cat_view.toString())
-        responseLiveData.observe(this, Observer {
-            val foodList = it.body()?.categories?.listIterator()
-            print(foodList)
-//            Log.i("MYTAGS", foodList)
-            if (foodList !=null) {
-//                var i = 0
-                while (foodList.hasNext()) {
-//                for (i in 0 .. 1){
-                    val foodItem = foodList.next()
-//                    val food_category_thumb = foodItem.strCategoryThumb
-
-//                    cat_title0.text = foodItem.strCategory
-//                    Glide.with(this)
-//                        .load(food_category_thumb)
-//                        .override(400, 350)
-//                        .fitCenter()
-//                        .fitCenter()
-//                        .into(cat_view0)
+//        responseLiveData.observe(this, Observer {
+//            val foodList = it.body()?.categories?.listIterator()
+//            print(foodList)
+////            Log.i("MYTAGS", foodList)
+//            if (foodList !=null) {
+////                var i = 0
+//                while (foodList.hasNext()) {
+////                for (i in 0 .. 1){
+//                    val foodItem = foodList.next()
+////                    val food_category_thumb = foodItem.strCategoryThumb
+//
+////                    cat_title0.text = foodItem.strCategory
+////
+////                }
+//
+//
+////                    val result:String = " Album title: ${foodItem.idCategory} \n"+
+////                            " Album Id: ${foodItem.strCategory} \n"+
+////                            " User Id: ${foodItem.strCategoryDescription} \n\n\n"
+//
+////                    Log.i("url", food_category_thumb)
+////                    text_view.append(result)
+////                    Glide.with(this)
+////                        .load(food_category_thumb)
+////                        .override(400, 350)
+////                        .fitCenter()
+////                        .fitCenter()
+////                        .into(cat_view0)
 //                }
-
-
-//                    val result:String = " Album title: ${foodItem.idCategory} \n"+
-//                            " Album Id: ${foodItem.strCategory} \n"+
-//                            " User Id: ${foodItem.strCategoryDescription} \n\n\n"
-
-//                    Log.i("url", food_category_thumb)
-//                    text_view.append(result)
-
 //            }
-                }
-            }
-        })
+//        })
     }
 
 
