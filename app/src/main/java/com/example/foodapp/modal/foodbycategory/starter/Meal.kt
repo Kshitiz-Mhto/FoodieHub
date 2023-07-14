e09c918f1dd798faedc99f73f666967617a97ffb
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-07-14 23:26:21 +0545

    serialized the variables for mapping for starter

--- app/src/main/java/com/example/foodapp/modal/foodbycategory/starter/Meal.kt
+++ app/src/main/java/com/example/foodapp/modal/foodbycategory/starter/Meal.kt
@@ -1,7 +1,12 @@
 package com.example.foodapp.modal.foodbycategory.starter
 
+import com.google.gson.annotations.SerializedName
+
 data class Meal(
+    @SerializedName("idMeal")
     val idMeal: String,
+    @SerializedName("strMeal")
     val strMeal: String,
+    @SerializedName("strMealThumb")
     val strMealThumb: String
 )
--- app/src/main/java/com/example/foodapp/modal/foodbycategory/starter/Starter.kt
+++ app/src/main/java/com/example/foodapp/modal/foodbycategory/starter/Starter.kt
@@ -1,5 +1,8 @@
 package com.example.foodapp.modal.foodbycategory.starter
 
+import com.google.gson.annotations.SerializedName
+
 data class Starter(
+    @SerializedName("meals")
     val meals: List<Meal>
 )
