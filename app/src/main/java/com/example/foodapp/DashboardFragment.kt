873b2dd81e73928471cb0f051caeb0d9c64ab1de
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-07-15 17:32:41 +0545

    added the adaptor for each categorical food item and the by default category to start with when dashboard pop ups

--- app/src/main/java/com/example/foodapp/DashboardFragment.kt
+++ app/src/main/java/com/example/foodapp/DashboardFragment.kt
@@ -2,23 +2,48 @@ package com.example.foodapp
 
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
-import com.example.foodapp.databinding.FragmentDashboardBinding
-import com.example.foodapp.modal.RetrofitInstance
-import com.example.foodapp.modal.category.FoodCategory
-import retrofit2.Response
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.foodapp.databinding.FragmentDashboardBinding
 import com.example.foodapp.endpoints.CategoryService
 import com.example.foodapp.endpoints.FoodsByCategory
+import com.example.foodapp.modal.RetrofitInstance
+import com.example.foodapp.modal.foodbycategory.Dessert.Dessert
+import com.example.foodapp.modal.foodbycategory.Dessert.DessertRecyclerViewAdaptor
+import com.example.foodapp.modal.foodbycategory.Goat.Goat
+import com.example.foodapp.modal.foodbycategory.Goat.GoatRecyclerViewAdaptor
+import com.example.foodapp.modal.foodbycategory.Lamb.Lamb
+import com.example.foodapp.modal.foodbycategory.Lamb.LambRecyclerViewAdaptor
+import com.example.foodapp.modal.foodbycategory.Pork.Pork
+import com.example.foodapp.modal.foodbycategory.Pork.PorkRecyclerViewAdaptor
+import com.example.foodapp.modal.foodbycategory.beef.Beef
+import com.example.foodapp.modal.foodbycategory.beef.BeefRecyclerViewAdaptor
+import com.example.foodapp.modal.foodbycategory.breakfast.Breakfast
+import com.example.foodapp.modal.foodbycategory.breakfast.BreakfastRecyclerViewAdaptor
+import com.example.foodapp.modal.foodbycategory.chicken.Chicken
+import com.example.foodapp.modal.foodbycategory.chicken.ChickenRecyclerViewAdaptor
+import com.example.foodapp.modal.foodbycategory.miscellaneous.Miscellaneous
+import com.example.foodapp.modal.foodbycategory.miscellaneous.MiscellaneousRecyclerViewAdaptor
+import com.example.foodapp.modal.foodbycategory.pasta.Pasta
+import com.example.foodapp.modal.foodbycategory.pasta.PastaRecyclerViewAdaptor
+import com.example.foodapp.modal.foodbycategory.seafood.Seafood
+import com.example.foodapp.modal.foodbycategory.seafood.SeafoodRecyclerViewAdaptor
+import com.example.foodapp.modal.foodbycategory.sides.Side
+import com.example.foodapp.modal.foodbycategory.sides.SidesRecyclerViewAdaptor
 import com.example.foodapp.modal.foodbycategory.starter.Starter
+import com.example.foodapp.modal.foodbycategory.starter.StarterRecyclerViewAdaptor
+import com.example.foodapp.modal.foodbycategory.vegan.Vegan
+import com.example.foodapp.modal.foodbycategory.vegan.VeganRecyclerViewAdaptor
+import com.example.foodapp.modal.foodbycategory.vegetarian.Vegetarian
+import com.example.foodapp.modal.foodbycategory.vegetarian.VegetarianRecyclerViewAdaptor
 import okhttp3.internal.toImmutableList
+import retrofit2.Response
 
 
 class DashboardFragment : Fragment() {
@@ -36,63 +61,205 @@ class DashboardFragment : Fragment() {
     ): View? {
 
         retService = RetrofitInstance.getRetrofitInstance().create(CategoryService::class.java)
-        retfoodsByCategory = RetrofitInstance.getRetrofitInstance().create(FoodsByCategory::class.java)
-        binding = FragmentDashboardBinding.inflate(inflater,container, false)
+        retfoodsByCategory =
+            RetrofitInstance.getRetrofitInstance().create(FoodsByCategory::class.java)
+        binding = FragmentDashboardBinding.inflate(inflater, container, false)
 
         val recyclerView = binding.dashRecyclerview
-//        getStarterwithoutparameter()
-
         recyclerView.setBackgroundColor(Color.TRANSPARENT)
-//        getRequestWithParameters()
         recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-        val responseLiveData: LiveData<Response<Starter>> = liveData {
-            val response = retfoodsByCategory.getStarter()
-            emit(response)
-        }
-        responseLiveData.observe(this, Observer {
-            val foodList = it.body()?.meals?.toImmutableList()
-            if (foodList != null) {
-                recyclerView.adapter = MyRecyclerViewAdaptor(foodList)
 
+        binding.root.postDelayed(Runnable {
+                binding.catStarter.performClick()
+            },
+            100)
+
+        binding.catStarter.setOnClickListener {
+            val responseLiveData: LiveData<Response<Starter>> = liveData {
+                val response = retfoodsByCategory.getStarter()
+                emit(response)
             }
-        })
+            responseLiveData.observe(this, Observer {
+                val foodList = it.body()?.meals?.toImmutableList()
+                if (foodList != null) {
+                    recyclerView.adapter = StarterRecyclerViewAdaptor(foodList)
+                }
+            })
+        }
+        binding.catBeef.setOnClickListener {
+            val responseLiveData: LiveData<Response<Beef>> = liveData {
+                val response = retfoodsByCategory.getBeef()
+                emit(response)
+            }
+            responseLiveData.observe(this, Observer {
+                val foodList = it.body()?.meals?.toImmutableList()
+                if (foodList != null) {
+                    recyclerView.adapter = BeefRecyclerViewAdaptor(foodList)
+                }
+            })
+        }
+        binding.catBreakfast.setOnClickListener {
+            val responseLiveData: LiveData<Response<Breakfast>> = liveData {
+                val response = retfoodsByCategory.getBreakfast()
+                emit(response)
+            }
+            responseLiveData.observe(this, Observer {
+                val foodList = it.body()?.meals?.toImmutableList()
+                if (foodList != null) {
+                    recyclerView.adapter = BreakfastRecyclerViewAdaptor(foodList)
+                }
+            })
+        }
+        binding.catChicken.setOnClickListener {
+            val responseLiveData: LiveData<Response<Chicken>> = liveData {
+                val response = retfoodsByCategory.getChicken()
+                emit(response)
+            }
+            responseLiveData.observe(this, Observer {
+                val foodList = it.body()?.meals?.toImmutableList()
+                if (foodList != null) {
+                    recyclerView.adapter = ChickenRecyclerViewAdaptor(foodList)
+                }
+            })
+        }
+        binding.catDessert.setOnClickListener {
+            val responseLiveData: LiveData<Response<Dessert>> = liveData {
+                val response = retfoodsByCategory.getDessert()
+                emit(response)
+            }
+            responseLiveData.observe(this, Observer {
+                val foodList = it.body()?.meals?.toImmutableList()
+                if (foodList != null) {
+                    recyclerView.adapter = DessertRecyclerViewAdaptor(foodList)
+                }
+            })
+        }
+        binding.catGoat.setOnClickListener {
+            val responseLiveData: LiveData<Response<Goat>> = liveData {
+                val response = retfoodsByCategory.getGoat()
+                emit(response)
+            }
+            responseLiveData.observe(this, Observer {
+                val foodList = it.body()?.meals?.toImmutableList()
+                if (foodList != null) {
+                    recyclerView.adapter = GoatRecyclerViewAdaptor(foodList)
+                }
+            })
+        }
+        binding.catLamb.setOnClickListener {
+            val responseLiveData: LiveData<Response<Lamb>> = liveData {
+                val response = retfoodsByCategory.getLamb()
+                emit(response)
+            }
+            responseLiveData.observe(this, Observer {
+                val foodList = it.body()?.meals?.toImmutableList()
+                if (foodList != null) {
+                    recyclerView.adapter = LambRecyclerViewAdaptor(foodList)
+                }
+            })
+        }
+        binding.catMiscellaneous.setOnClickListener {
+            val responseLiveData: LiveData<Response<Miscellaneous>> = liveData {
+                val response = retfoodsByCategory.getMiscellaneous()
+                emit(response)
+            }
+            responseLiveData.observe(this, Observer {
+                val foodList = it.body()?.meals?.toImmutableList()
+                if (foodList != null) {
+                    recyclerView.adapter = MiscellaneousRecyclerViewAdaptor(foodList)
+                }
+            })
+        }
+        binding.catPasta.setOnClickListener {
+            val responseLiveData: LiveData<Response<Pasta>> = liveData {
+                val response = retfoodsByCategory.getPasta()
+                emit(response)
+            }
+            responseLiveData.observe(this, Observer {
+                val foodList = it.body()?.meals?.toImmutableList()
+                if (foodList != null) {
+                    recyclerView.adapter = PastaRecyclerViewAdaptor(foodList)
+                }
+            })
+        }
+        binding.catPork.setOnClickListener {
+            val responseLiveData: LiveData<Response<Pork>> = liveData {
+                val response = retfoodsByCategory.getPork()
+                emit(response)
+            }
+            responseLiveData.observe(this, Observer {
+                val foodList = it.body()?.meals?.toImmutableList()
+                if (foodList != null) {
+                    recyclerView.adapter = PorkRecyclerViewAdaptor(foodList)
+                }
+            })
+        }
+        binding.catSeafood.setOnClickListener {
+            val responseLiveData: LiveData<Response<Seafood>> = liveData {
+                val response = retfoodsByCategory.getSeafood()
+                emit(response)
+            }
+            responseLiveData.observe(this, Observer {
+                val foodList = it.body()?.meals?.toImmutableList()
+                if (foodList != null) {
+                    recyclerView.adapter = SeafoodRecyclerViewAdaptor(foodList)
+                }
+            })
+        }
+        binding.catSide.setOnClickListener {
+            val responseLiveData: LiveData<Response<Side>> = liveData {
+                val response = retfoodsByCategory.getSide()
+                emit(response)
+            }
+            responseLiveData.observe(this, Observer {
+                val foodList = it.body()?.meals?.toImmutableList()
+                if (foodList != null) {
+                    recyclerView.adapter = SidesRecyclerViewAdaptor(foodList)
+                }
+            })
+        }
+        binding.catVegan.setOnClickListener {
+            val responseLiveData: LiveData<Response<Vegan>> = liveData {
+                val response = retfoodsByCategory.getVegan()
+                emit(response)
+            }
+            responseLiveData.observe(this, Observer {
+                val foodList = it.body()?.meals?.toImmutableList()
+                if (foodList != null) {
+                    recyclerView.adapter = VeganRecyclerViewAdaptor(foodList)
+                }
+            })
+        }
+        binding.catVegetarian.setOnClickListener {
+            val responseLiveData: LiveData<Response<Vegetarian>> = liveData {
+                val response = retfoodsByCategory.getVegetarian()
+                emit(response)
+            }
+            responseLiveData.observe(this, Observer {
+                val foodList = it.body()?.meals?.toImmutableList()
+                if (foodList != null) {
+                    recyclerView.adapter = VegetarianRecyclerViewAdaptor(foodList)
+                }
+            })
+        }
 
         return binding.root
     }
-
-    private fun getStarterwithoutparameter(){
-        val responseLiveData: LiveData<Response<Starter>> = liveData {
-            val response = retfoodsByCategory.getStarter()
-//            println(response.body())
-//            println("0000000000000000000000000000000000")
-            emit(response)
-        }
-
-        responseLiveData.observe(this, Observer {
-            val starterList = it.body()?.meals?.listIterator()
-            print(starterList)
-            Log.i("MYTAGS", starterList.toString())
-            if (starterList != null) {
-//                binding..text = starterList.next().strMeal
-                var starterList101 = starterList
-            }
-        })
+}
 
 
-    }
-
-    private fun getRequestWithParameters() {
-
-        val responseLiveData: LiveData<Response<FoodCategory>> = liveData {
-            val response = retService.getCategory()
-            emit(response)
-        }
-        responseLiveData.observe(this, Observer {
-            val foodList = it.body()?.categories?.listIterator()
-            if (foodList != null) {
-//                foodList
-            }
-        })
+//    private fun getRequestWithParameters() {
+//
+//        val responseLiveData: LiveData<Response<FoodCategory>> = liveData {
+//            val response = retService.getCategory()
+//            emit(response)
+//        }
+//        responseLiveData.observe(this, Observer {
+//            val foodList = it.body()?.categories?.listIterator()
+//            if (foodList != null) {
+////                foodList
+//            }
+//        })
 //        return null
 
 //        var cat_view0  = binding.catThumb0
@@ -130,7 +297,7 @@ class DashboardFragment : Fragment() {
 //                }
 //            }
 //        })
-    }
+//    }
 
 
-}
+//}
