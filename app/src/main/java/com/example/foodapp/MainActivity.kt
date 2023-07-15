5c125ff62e673870d51c7daedd0133e3a457d1cb
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-07-16 01:31:01 +0545

    removed unused packages

--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.foodapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
