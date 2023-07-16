9ce1b59c3cd01a119837580005806380d5ac8046
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-07-16 14:39:48 +0545

    added endpoint for search by food category

--- app/src/main/java/com/example/foodapp/endpoints/SearchByName.kt
+++ app/src/main/java/com/example/foodapp/endpoints/SearchByName.kt
@@ -1,4 +1,14 @@
 package com.example.foodapp.endpoints
 
+import com.example.foodapp.modal.foodbysearch.FoodBySearch
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface SearchByName {
+
+    // url because query name should not be static in url.
+    @GET("/api/json/v1/1/search.php")
+    abstract suspend fun getFoodBySearch(@Query("s") food_name:String): Response<FoodBySearch>
+
 }
