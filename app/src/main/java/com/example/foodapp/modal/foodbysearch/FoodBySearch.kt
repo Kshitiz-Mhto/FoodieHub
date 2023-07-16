5f6ac7cd83b0a189742c7f0eed9b3cc18ea2ff5e
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-07-16 14:37:50 +0545

    resolved dependency

---
+++ app/src/main/java/com/example/foodapp/modal/foodbysearch/FoodBySearch.kt
@@ -0,0 +1,5 @@
+package com.example.foodapp.modal.foodbysearch
+
+data class FoodBySearch(
+    val meals: List<Meal>
+)
---
+++ app/src/main/java/com/example/foodapp/modal/foodbysearch/FoodSearchRecyclerViewAdaptor.kt
@@ -0,0 +1,4 @@
+package com.example.foodapp.modal.foodbysearch
+
+class FoodSearchRecyclerViewAdaptor {
+}
