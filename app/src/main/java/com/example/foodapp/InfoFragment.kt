c7d8d385397a9177133213c32823c38f37a07c3e
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2023-07-17 21:51:57 +0545

    added shared preferences feature to receive the data from recyclerview adaptor

--- app/src/main/java/com/example/foodapp/InfoFragment.kt
+++ app/src/main/java/com/example/foodapp/InfoFragment.kt
@@ -1,16 +1,35 @@
 package com.example.foodapp
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import com.bumptech.glide.Glide
 import com.example.foodapp.databinding.FragmentInfoBinding
+import com.example.foodapp.endpoints.SearchByName
+import com.example.foodapp.modal.RetrofitInstance
+import com.example.foodapp.modal.foodbysearch.FoodBySearch
+import okhttp3.internal.toImmutableList
+import retrofit2.Response
 
 class InfoFragment : Fragment() {
+
     private lateinit var binding: FragmentInfoBinding
+    private lateinit var retFoodBySearch: SearchByName
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var foodName: String
+    private lateinit var foodUrl: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -18,6 +37,33 @@ class InfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentInfoBinding.inflate(inflater, container, false)
+        retFoodBySearch = RetrofitInstance.getRetrofitInstance()
+            .create(SearchByName::class.java)
+
+        // Retrieve data from SharedPreferences
+        sharedPreferences = requireActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        foodName = sharedPreferences.getString("foodName", "") ?: ""
+        foodUrl = sharedPreferences.getString("foodUrl", "") ?: ""
+        Log.i("name", foodName)
+
+
+        val responseLiveData: LiveData<Response<FoodBySearch>> = liveData {
+            val response = retFoodBySearch.getFoodBySearch(foodName)
+            Log.i("response", response.toString())
+            emit(response)
+        }
+        responseLiveData.observe(this, Observer {
+            val foodList = it.body()?.meals?.toImmutableList()
+            if (foodList != null) {
+                binding.etFood.text = foodName
+                binding.etDescriptionDetail.text = foodList[0].strInstructions
+                Glide.with(this.context)
+                    .load(foodUrl)
+                    .into(binding.etImage)
+            }
+        })
+
+
         return binding.root
     }
 }
